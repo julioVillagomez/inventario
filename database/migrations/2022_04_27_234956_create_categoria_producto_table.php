bd49be1f8f84729746aa9b5af4ecabb64d113533
Julio Cesar Villagomez <julio@pitz.io>
2022-04-27 18:54:37 -0500

    punto 3 de examen

---
+++ database/migrations/2022_04_27_234956_create_categoria_producto_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Categoria;
+use App\Models\Producto;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categoria_producto', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Categoria::class)->constrained();
+            $table->foreignIdFor(Producto::class)->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categoria_producto');
+    }
+};
